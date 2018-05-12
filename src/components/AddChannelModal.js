24af6cf6b9fee1e0f46aa985322622880357eac1
ivanwarke <iveon.cc@gmail.com>
2018-05-12 13:02:04 +0800

    [Add] Channel Modal

--- src/components/AddChannelModal.js
+++ src/components/AddChannelModal.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Header, Modal } from 'semantic-ui-react';
+import { Input, Form, Button, Modal } from 'semantic-ui-react';
 
-const ModalModalExample = ({ open, onClose }) => (
+import '../styles/modal.css';
+
+const AddChannelModal = ({ open, onClose }) => (
   <Modal open={open} onClose={onClose}>
-    <Modal.Header>Select a Photo</Modal.Header>
-    <Modal.Content image>
-      <Modal.Description>
-        <Header>Default Profile Image</Header>
-        <p>We’ve found the following gravatar image associated with your e-mail address.</p>
-        <p>Is it okay to use this photo?</p>
-      </Modal.Description>
+    <Modal.Header>Add Channel</Modal.Header>
+    <Modal.Content>
+      <Form>
+        <Form.Field>
+          <Input fluid placeholder="Channel name" />
+        </Form.Field>
+        <Form.Group>
+          <Button fluid>Cancel</Button>
+          <Button fluid>Create Channel</Button>
+        </Form.Group>
+      </Form>
     </Modal.Content>
   </Modal>
 );
 
-export default ModalModalExample;
+export default AddChannelModal;
