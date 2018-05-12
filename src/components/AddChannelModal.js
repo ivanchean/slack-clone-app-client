f431f85f41b04ab4080c9a904bf3cdee1756fa6e
ivanwarke <iveon.cc@gmail.com>
2018-05-12 23:01:23 +0800

    [Update] Channel Modal

--- src/components/AddChannelModal.js
+++ src/components/AddChannelModal.js
@@ -3,8 +3,10 @@ import { Input, Form, Button, Modal } from 'semantic-ui-react';
 import { withFormik } from 'formik';
 import gql from 'graphql-tag';
 import { compose, graphql } from 'react-apollo';
+import findIndex from 'lodash/findIndex';
 
 import '../styles/modal.css';
+import { allTeamsQuery } from '../graphql/team';
 
 const AddChannelModal = ({
   open,
@@ -40,7 +42,13 @@ const AddChannelModal = ({
 
 const createChannelMutation = gql`
   mutation($teamId: Int!, $name: String!) {
-    createChannel(teamId: $teamId, name: $name)
+    createChannel(teamId: $teamId, name: $name) {
+      ok
+      channel {
+        id
+        name
+      }
+    }
   }
 `;
 
@@ -49,9 +57,23 @@ export default compose(
   withFormik({
     mapPropsToValues: () => ({ name: '' }),
     handleSubmit: async (values, { props: { onClose, teamId, mutate }, setSubmitting }) => {
-      await mutate({ variables: { teamId, name: values.name } });
+      await mutate({
+        variables: { teamId, name: values.name },
+        update: (proxy, { data: { createChannel } }) => {
+          const { ok, channel } = createChannel;
+          if (!ok) {
+            return;
+          }
+
+          const data = proxy.readQuery({ query: allTeamsQuery });
+          console.log(data);
+          const teamIdx = findIndex(data.allTeams, ['id', teamId]);
+          data.allTeams[teamIdx].channels.push(channel);
+          proxy.writeQuery({ query: allTeamsQuery, data });
+        },
+      });
       onClose();
-      setSubmitting();
+      setSubmitting(false);
     },
   }),
 )(AddChannelModal);
--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
 import findIndex from 'lodash/findIndex';
 import decode from 'jwt-decode';
 
 import Channels from '../components/Channels';
 import Teams from '../components/Teams';
 import AddChannelModal from '../components/AddChannelModal';
-
-const allTeamsQuery = gql`
-  {
-    allTeams {
-      id
-      name
-      channels {
-        id
-        name
-      }
-    }
-  }
-`;
+import { allTeamsQuery } from '../graphql/team';
 
 class Sidebar extends React.Component {
   state = {
@@ -70,12 +57,13 @@ class Sidebar extends React.Component {
               key="channels-sidebar"
               teamName={team.name}
               username={username}
+              teamId={team.id}
               channels={team.channels}
               users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
               onAddChannelClick={this.handleAddChannelClick}
             />,
             <AddChannelModal
-              teamId={currentTeamId}
+              teamId={team.id}
               onClose={this.handleCloseAddChannelModal}
               open={this.state.openAddChannelModal}
               key="sidebar-add-channel-modal"
