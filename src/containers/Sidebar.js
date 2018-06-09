4c077612287c4a5af6b98fa848a8ed8535d2e51e
ivanwarke <iveon.cc@gmail.com>
2018-06-09 18:33:07 +0800

    [Fix] eslint

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import decode from 'jwt-decode';
+// import decode from 'jwt-decode';
 
 import Channels from '../components/Channels';
 import Teams from '../components/Teams';
